d8c96fe0fa0c20c575c9abda37102ffe0aff660c
mithiladongre <mithiladongre6@gmail.com>
2024-06-27 17:22:03 +0530

    Quickverse develop (#8)
    * Add image files
    
    * Add files via upload
    
    * Deleted a5.jpeg from the repository
    
    * Updated SignUp.tsx with new validation logic
    
    ---------
    
    Co-authored-by: Abhilashghope <33655535+Abhilashghope@users.noreply.github.com>

--- src/Components/Login/SignUp.tsx
+++ src/Components/Login/SignUp.tsx
@@ -20,6 +20,7 @@ import {fetchCampusIds} from '../../services/fetchCampusIds';
 const SignupScreen: React.FC = () => {
   const [fullName, setFullName] = useState<string>('');
   const [pin, setPin] = useState<string>('');
+  const [confirmPin,setconfirmPin] = useState<string>('');
   const [phoneNumber, setPhoneNumber] = useState<string>('');
   const [email, setEmail] = useState<string>('');
   const [error, setError] = useState<string>('');
@@ -39,6 +40,17 @@ const SignupScreen: React.FC = () => {
       isLoadingCampuses(false);
     }
   };
+  const handlingconfirmPin = (value: string) => {
+    setconfirmPin(value); 
+  
+    if (pin !== value) {
+      setError('Confirmation pin does not match the original pin');
+    } 
+    else {
+      setError('');
+    }
+  };
+  
   const validateFields = () => {
     let isValid = true;
     if (
@@ -46,6 +58,7 @@ const SignupScreen: React.FC = () => {
       !phoneNumber.match(/^\d{10}$/) ||
       !email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/) ||
       !pin.match(/^\d{4}$/) ||
+      pin!= confirmPin ||
       !selectedCampusId
     ) {
       setError('Please fill out all fields correctly.');
@@ -54,6 +67,7 @@ const SignupScreen: React.FC = () => {
     // setError('');
     return isValid;
   };
+  
   const auth = useAuth();
   const signUp = async () => {
     if (validateFields()) {
@@ -147,6 +161,24 @@ const SignupScreen: React.FC = () => {
           placeholder="4-digit Pin"
           value={pin}
           onChangeText={setPin}
+          placeholderTextColor={theme.colors.ternary}
+          secureTextEntry
+          keyboardType="numeric"
+          maxLength={4}
+        />
+      </View>
+      <View style={styles.inputContainer}>
+        <MaterialCommunityIcons
+          name="lock-check"
+          size={24}
+          color={theme.colors.ternary}
+          style={styles.icon}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="confirm Pin"
+          value={confirmPin}
+          onChangeText={handlingconfirmPin}
           placeholderTextColor={theme.colors.ternary}
           secureTextEntry
           keyboardType="numeric"

c69cc08ff235bc3794fb9aa6c558378668bf1eb1
Abhilashghope <abhilashghope111@gmail.com>
2024-07-03 18:49:43 +0530

    handle login errors

--- src/Components/Vendors/VendorsNavigator.tsx
+++ src/Components/Vendors/VendorsNavigator.tsx
@@ -32,10 +32,7 @@ const VendorsNavigator: React.FC = () => {
           headerStyle: {
             backgroundColor: theme.colors.primary,
           },
-          title:
-            authData && authData.session
-              ? authData?.session?.campus
-              : 'Place an Order',
+          title: authData ? authData?.session?.campus : 'Place an Order',
         }}
       />
     </Stack.Navigator>

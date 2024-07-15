72a87a99fc42b1ccd75f43d7343891d29ee32662
Abhilashghope <33655535+Abhilashghope@users.noreply.github.com>
2024-07-15 23:53:59 +0530

    aboutUs (#25)

--- src/Components/UserProfile/AboutUs.tsx
+++ src/Components/UserProfile/AboutUs.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Image} from 'react-native';
+import theme from '../../theme';
 
 const AboutUs = () => {
   return (
     <View style={styles.container}>
-      <Text>My Orders Screen</Text>
+      <Image
+        // eslint-disable-next-line prettier/prettier, no-useless-escape
+        source={require('../../data/images/qv-blue.png')}
+        style={styles.profileImage}
+      />
+      <View style={styles.container2}>
+        <Text style={styles.font}>
+          QuickVerse is a hyper-local delivery startup for India's premium
+          campuses, utilizing advanced drone technology for swift and reliable
+          delivery of groceries, meals, books, and more. Our mission is to
+          enhance campus life by providing fast, efficient, and eco-friendly
+          delivery solutions tailored to the unique needs of each community.
+        </Text>
+      </View>
     </View>
   );
 };
@@ -13,7 +27,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    backgroundColor: theme.colors.primary,
+    padding: 20,
+    fontSize: 22,
+  },
+  font: {
+    fontSize: 22,
+    color: theme.colors.ternary,
+  },
+  profileImage: {
+    width: 200,
+    height: 200,
+  },
+  container2: {
+    marginTop: 60,
   },
 });
 

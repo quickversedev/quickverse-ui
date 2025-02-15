627f5912e650f3b03ed7e03adb8c0f0907f79971
Abhilashghope <abhilashghope111@gmail.com>
2025-02-15 22:13:49 +0530

    eslint erro

--- src/Components/util/vendorUtil.ts
+++ src/Components/util/vendorUtil.ts
@@ -10,8 +10,12 @@ export const isStoreOpen = (
     const [hours, minutes] = time.split(':').map(Number);
 
     let hour = hours;
-    if (period.toUpperCase() === 'PM' && hour !== 12) hour += 12;
-    if (period.toUpperCase() === 'AM' && hour === 12) hour = 0;
+    if (period.toUpperCase() === 'PM' && hour !== 12) {
+      hour += 12;
+    }
+    if (period.toUpperCase() === 'AM' && hour === 12) {
+      hour = 0;
+    }
 
     return hour * 60 + minutes;
   };
@@ -22,7 +26,9 @@ export const isStoreOpen = (
   // Handle overnight closing time (e.g., 6:30 PM - 2:30 AM)
   if (closingMinutes < openingMinutes) {
     closingMinutes += 24 * 60;
-    if (currentTime < openingMinutes) currentTime += 24 * 60;
+    if (currentTime < openingMinutes) {
+      currentTime += 24 * 60;
+    }
   }
 
   return currentTime >= openingMinutes && currentTime <= closingMinutes;

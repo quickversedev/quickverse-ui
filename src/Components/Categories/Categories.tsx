8cf29bf8cd42b2dc69000d7a4d2f2f24c552f451
quickversedev <qvdev@quickverse.in>
2025-05-11 22:36:58 +0530

    Release/qv 2.0 (#120)
    * version change
    
    * final touch/webview-close event lister changes (#109)
    
    * Updating Prd endpoint for IOS
    
    * feedback (#113)
    
    * Feedback1 (#114)
    
    * feedback
    
    * devrement iussue fix
    
    * Feedback1 (#115)
    
    * feedback
    
    * devrement iussue fix
    
    * selectCategorie fix and vendor page fix
    
    ---------
    
    Co-authored-by: Abhilashghope <abhilashghope111@gmail.com>
    Co-authored-by: Abhilashghope <33655535+Abhilashghope@users.noreply.github.com>
    
    * Feedback1 (#116)
    
    * feedback
    
    * devrement iussue fix
    
    * selectCategorie fix and vendor page fix
    
    * vendor card fix
    
    ---------
    
    Co-authored-by: Abhilashghope <abhilashghope111@gmail.com>
    Co-authored-by: Abhilashghope <33655535+Abhilashghope@users.noreply.github.com>
    
    * Feedback1 (#117)
    
    * feedback
    
    * devrement iussue fix
    
    * selectCategorie fix and vendor page fix
    
    * vendor card fix
    
    * vendor card fix
    
    ---------
    
    Co-authored-by: Abhilashghope <abhilashghope111@gmail.com>
    Co-authored-by: Abhilashghope <33655535+Abhilashghope@users.noreply.github.com>
    
    * Feedback1 (#118)
    
    * feedback
    
    * devrement iussue fix
    
    * selectCategorie fix and vendor page fix
    
    * vendor card fix
    
    * vendor card fix
    
    * login fixes
    
    ---------
    
    Co-authored-by: Sagar <sagarkhatri8950@gmail.com>
    Co-authored-by: Sagar Khatri <35953417+sagarkhatri8950@users.noreply.github.com>
    
    * Revert "Merge branch 'mainV2' into release/qv-2.0"
    
    This reverts commit e85a9bcdf0902e222bc72af525d13b9efa2f1adb, reversing
    changes made to a7d18aafc4263fdea610344e30218a3d3d8b34c2.
    
    ---------
    
    Co-authored-by: Abhilashghope <abhilashghope111@gmail.com>
    Co-authored-by: Abhilashghope <33655535+Abhilashghope@users.noreply.github.com>
    Co-authored-by: Sagar <sagarkhatri8950@gmail.com>
    Co-authored-by: Sagar Khatri <35953417+sagarkhatri8950@users.noreply.github.com>

--- src/Components/Categories/Categories.tsx
+++ src/Components/Categories/Categories.tsx
@@ -134,11 +134,7 @@ const Categories: React.FC<CategoriesScreenProps> = ({ route }) => {
 
   );
   const [searchQuery, setSearchQuery] = useState('');
-  useEffect(() => {
-    if (categoriesWithProducts.length > 0) {
-      setSelectedCategory(categoriesWithProducts[0]?.id);
-    }
-  });
+
 
 
   useEffect(() => {
